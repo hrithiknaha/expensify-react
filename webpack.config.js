4f649d607256d0365318063d04cf410800a89b89
Hrithik Naha <naha.hrithkk@gmail.com>
2019-09-23 16:04:46 +0530

    Setup Production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,51 @@
 //Webpack will grab this module to run, hence it is important to export it
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {   //Will create the Bundle.js file mainly for production
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin('styles.css')
+    return {
+        entry: "./src/app.js",
+        output: {   //Will create the Bundle.js file mainly for production
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: '#source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'), //Will run the webpack-dev-server which does not create the bundle file and runs it from memory.
-        historyApiFallback: true //Tells the server that wed be using client server routes and send the index.html for every error
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader : MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'sourve-map' : 'inline-souce-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'), //Will run the webpack-dev-server which does not create the bundle file and runs it from memory.
+            historyApiFallback: true //Tells the server that wed be using client server routes and send the index.html for every error
+        }
     }
 }
+
 
